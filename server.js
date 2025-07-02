37f653972e6643d90d1862526e75b3ac63b4f3cd
mitsshivashish <23it10sh63@mitsgwl.ac.in>
2025-07-02 22:17:41 +0530

    modified server.js to use https request

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback',
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback',
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
