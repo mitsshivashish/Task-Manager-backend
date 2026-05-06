ca7edf869f0285a1a14c8692d2c9c86cda5403af
mitsshivashish <23it10sh63@mitsgwl.ac.in>
2026-05-07 03:30:00 +0530

    added logs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-const sendEmail = async (to, subject, text , html) => {
-  const mailOptions = {
+const sendEmail = async (to, subject, text, html) => {
+  await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
     subject,
     text,
     html,
-  };
-  await transporter.sendMail(mailOptions);
+  });
 };
+
+module.exports = sendEmail;
 
 module.exports = sendEmail; 
